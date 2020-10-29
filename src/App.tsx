3bae83e8bbc83bdd35592bfd89418c9d8033ed32
Matheus Braz <matheus.braz14@gmail.com>
2020-10-29 15:43:45 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div style={{position: 'absolute', height: '100%', width: '100%', margin: 0}}>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
